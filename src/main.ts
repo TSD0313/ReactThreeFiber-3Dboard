5c0201de863e41411a681abe72701e388968edba
TSD0313 <57118279+TSD0313@users.noreply.github.com>
2020-07-09 17:25:44 +0900

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -14,72 +14,86 @@ window.onload = function() {
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
+    // renderer.setClearColor("#FFFFFF");
     // レンダラー側で影を有効に
     renderer.shadowMap.enabled = true;
 
     // シーンを作成
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color("#BBFFFF");
 
     // カメラを作成
-    const camera = new THREE.PerspectiveCamera(60, width / height);
-    camera.position.set( 0, 800, 900 );
+    const camera = new THREE.PerspectiveCamera(70, width / height);
+    camera.position.set( 0, 900, 900 );
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     // 光源を作成
-    {
-        const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
-        scene.add(light);
-    }
+    const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+    light.castShadow = true;
+    scene.add(light);
+    light.position.set( 0, 450, 0 );
+    light.lookAt(0,0,0)
+    
 
     // ボードを作成
     {
-    const board =  new THREE.Mesh(                                      
-        new THREE.PlaneGeometry(1200, 800, 8, 4),
-        new THREE.MeshLambertMaterial({ 
-        color: "white",
-        wireframe: true         
-    }));
-    board.rotation.x = -Math.PI / 2;
-    scene.add(board);
+        const boardBack =  new THREE.Mesh(                                      
+            new THREE.PlaneGeometry(1600, 800),
+            new THREE.MeshStandardMaterial({ 
+            color: "palegreen"   
+            })
+        );
+        boardBack.rotation.x = -Math.PI / 2;
+
+        const boardLine =  new THREE.Mesh(                                      
+            new THREE.PlaneGeometry(1600, 800, 8, 4),
+            new THREE.MeshLambertMaterial({ 
+            color: "red",
+            wireframe: true    
+        }));
+        boardLine.rotation.x = -Math.PI / 2;
+
+        const boardGroup = new THREE.Group()
+        boardGroup.add(boardBack,boardLine);
+        boardGroup.receiveShadow = true;
+        scene.add(boardGroup);
     }
-    
 
     const genCardImgObj = (openImg:string)=>{
         const planeCard = new THREE.PlaneGeometry(123, 180);
         const openTexture = new THREE.TextureLoader().load(openImg);
-        const backTexture = new THREE.TextureLoader().load('cardback.jpg');
+        const backTexture = new THREE.TextureLoader().load("cardback.jpg");
+
         const openMaterial = new THREE.MeshStandardMaterial({
             map: openTexture
         });
         const backMaterial = new THREE.MeshStandardMaterial({
             map: backTexture
         });
-        const sumpleCardOpen = new THREE.Mesh(planeCard, openMaterial);
-        const sumpleCareBack = new THREE.Mesh(planeCard, backMaterial);
-        sumpleCareBack.rotation.y = -Math.PI;
+
+        const sampleCardOpen = new THREE.Mesh(planeCard, openMaterial);
+        const sampleCareBack = new THREE.Mesh(planeCard, backMaterial);
+        sampleCareBack.rotation.y = -Math.PI;
         const wrap = new THREE.Group(); 
-        wrap.add(sumpleCardOpen,sumpleCareBack);
+        wrap.add(sampleCardOpen,sampleCareBack);
+        wrap.rotation.x = -Math.PI/2;
+        wrap.castShadow = true;
+        wrap.receiveShadow = true;
         return wrap;
     };
-    
+
     const alphaA = genCardImgObj("alpha.jpg");
-    alphaA.rotation.z = -Math.PI/4;
-    alphaA.position.set(0,120,0);
+    alphaA.position.set(100,2,100);
     scene.add(alphaA);
 
     const alphaB = genCardImgObj("alpha.jpg");
-    alphaB.rotation.x = -Math.PI/2;
-    alphaB.position.set(0,0,0);
+    alphaB.position.set(-300,2,100);
     scene.add(alphaB);
 
-    const alphaC = genCardImgObj("alpha.jpg");
-    alphaC.rotation.x = -Math.PI/2;
-    alphaC.position.set(-225,0,300);
-    scene.add(alphaC);
+    const cards = [alphaA,alphaB]
 
     const mouse = new THREE.Vector2();
     mainCanv.addEventListener('mousemove', handleMouseMove);
-    // マウスを動かしたときのイベント
     function handleMouseMove(event) {
         const element = event.currentTarget;
         // canvas要素上のXY座標
@@ -96,27 +110,35 @@ window.onload = function() {
 
     const raycaster = new THREE.Raycaster();
 
+    const flipAnimation = (Obj:THREE.Object3D) => {
+        createjs.Tween.get(Obj.position)
+            .to({y:Obj.position.y+150},250)
+            .to({y:Obj.position.y},250);
+        Tween.get(Obj.rotation)
+            .to({y:(-Math.PI-Obj.rotation.y),z:(-Math.PI/2-Obj.rotation.z)},500);
+        return
+    };
+
+    // const floatupAnimation = (Obj:THREE.Object3D) => {
+    //     createjs.Tween.get(Obj.position)
+    //         .to({y:Obj.position.y+50},250)
+    //     return
+    // };
+
     renderer.domElement.addEventListener("click", onclick, true);
     function onclick(event) {
         raycaster.setFromCamera(mouse, camera);
-        var intersects = raycaster.intersectObjects(scene.children); //array
+        const intersects = raycaster.intersectObjects(cards,true); 
         if (intersects.length > 0) {
-            const selectedObject = intersects[0].object;
-            selectedObject.position.y = 100
-        }
-    }
+            const selectedObject = intersects[0].object.parent;
+            flipAnimation(selectedObject);
+        };
+    };
 
     tick();
 
     // 毎フレーム時に実行されるループイベント
     function tick() {
-      // 角度に応じてカメラの位置を設定
-      camera.position.x = 800 * Math.sin(Date.now() / 1500);
-      camera.position.y = 500;
-      camera.position.z = 800 * Math.cos(Date.now() / 1500);
-      // 原点方向を見つめる
-      camera.lookAt(new THREE.Vector3(0, 0, 0));
-
       // レンダリング
       renderer.render(scene, camera);
       requestAnimationFrame(tick);
